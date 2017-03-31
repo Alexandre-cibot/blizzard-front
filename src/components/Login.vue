099636e46b37bfec817bc6448e4e57a9f3d9642f
Alexandre Cibot <alexandre.cibot@gmail.com>
2017-03-31 13:49:56 +0200

    Adding login page view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,41 @@
 <template>
-  <div class="login">
-    <h1>{{msg}}</h1>
+  <div class="hero is-fullheight">
+      <div class="login-page-wrapper hero-body">
+        <div class="container">
+          <div class="columns is-vcentered">
+            <div class="column is-4 is-offset-4">
+              <h1 class="title">
+                Comparateur Diablo 3
+              </h1>
+              <div class="box">
+                <label class="label">Battle.net nickname</label>
+                <p class="control">
+                  <input class="input" type="text" placeholder="jsmith@example.org">
+                </p>
+                <label class="label">Battle.net password</label>
+                <p class="control">
+                  <input class="input" type="password" placeholder="●●●●●●●">
+                </p>
+                <hr>
+                <p class="control">
+                  <button class="button is-primary">Login</button>
+                  <button class="button is-default">Cancel</button>
+                </p>
+              </div>
+              <p class="has-text-centered">
+                <a href="https://eu.battle.net/d3/fr/">Register on official website</a>
+                |
+                <a href="https://github.com/Alexandre-cibot/blizzard-front/">View GitHub</a>
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
   </div>
+
+  </section>
 </template>
 
 <script>
@@ -16,4 +50,9 @@ export default {
 </script>
 
 <style lang="css">
+.login-page-wrapper {
+  /*background-image: url('../assets/img/bg_diablo.jpg');*/
+  background-position: center;
+  background-repeat: no-repeat;
+}
 </style>
